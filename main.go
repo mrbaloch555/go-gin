3089415777d2bdaa17ed6a15d93b76454c6f7c11
durrahkhan <durrah.khan@camp1.tkexl.com>
2023-07-12 01:29:42 +0500

    initials commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello world",
+		})
+	})
+
+	router.Run(":3000")
+}
